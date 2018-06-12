7a121e9eb8ffc1ec23a66f7d27f7f14ede9a3276
nanobot2000 <marginal3452@yandex.ru>
2018-06-12 20:44:26 +0300

    fix issues

--- image_resize.py
+++ image_resize.py
@@ -7,22 +7,21 @@ from PIL import Image
 def resize_image(inputfile, outputdir, width, height, scale):
     im = Image.open(inputfile)
     old_width, old_height = im.size
-    old_dir = os.path.split(inputfile)[0]
     old_filename, extension = os.path.split(inputfile)[-1].split('.')
     if scale and width is None and height is None:
-        new_width, new_height = old_width * scale, old_height * scale
+        new_width, new_height = int(old_width * scale), int(old_height * scale)
     elif width and height and scale is None:
         new_width, new_height = width, height
         if width / old_width != height / old_height:
             print('New image has different scale')
     elif width and scale is None:
         scale = width / old_width
-        new_width, new_height = old_width * scale, old_height * scale
+        new_width, new_height = int(old_width * scale), int(old_height * scale)
     elif height and scale is None:
         scale = height / old_height
-        new_width, new_height = old_width * scale, old_height * scale
+        new_width, new_height = int(old_width * scale), int(old_height * scale)
     else:
-        sys.exit('Incorrect or no arguments provided!')
+        return None, None
     new_name = '{old_filename}__{new_width}x{new_height}.{extension}'.format(
         old_filename=old_filename,
         new_width=new_width,
@@ -30,12 +29,7 @@ def resize_image(inputfile, outputdir, width, height, scale):
         extension=extension
     )
     im = im.resize(size=(new_width, new_height))
-    if outputdir:
-        im.save(os.path.join(outputdir, new_name))
-        print('Image is saved as {}'.format(os.path.join(outputdir, new_name)))
-    else:
-        im.save(os.path.join(old_dir, new_name))
-        print('Image is saved as {}'.format(os.path.join(old_dir, new_name)))
+    return im, new_name
 
 
 def create_argparser():
@@ -58,7 +52,7 @@ def create_argparser():
     parser.add_argument(
         '--scale',
         help='scale of the image',
-        type=int,
+        type=float,
     )
     parser.add_argument(
         '--outputdir',
@@ -72,6 +66,26 @@ if __name__ == '__main__':
     argparser = create_argparser()
     kwargs = vars(argparser)
     if os.path.isfile(kwargs['inputfile']):
-        resize_image(**kwargs)
+        resized_image, resized_image_name = resize_image(**kwargs)
     else:
         print("File doesn't exist!")
+    if resized_image is None:
+        sys.exit('Incorrect or no arguments provided')
+    old_dir = os.path.split(kwargs['inputfile'])[0]
+    outputdir = kwargs['outputdir']
+    if kwargs['outputdir']:
+        resized_image.save(os.path.join(outputdir, resized_image_name))
+        print('Image is saved as {}'.format(
+            os.path.join(
+                outputdir,
+                resized_image_name
+            )
+        ))
+    else:
+        resized_image.save(os.path.join(old_dir, resized_image_name))
+        print('Image is saved as {}'.format(
+            os.path.join(
+                old_dir,
+                resized_image_name
+            )
+        ))
