86a498099ee5cf1aa3addc07e7604527a1193590
nanobot2000 <marginal3452@yandex.ru>
2018-06-13 22:42:51 +0300

    code refractoring, split function resize_image on subfunctions

--- image_resize.py
+++ image_resize.py
@@ -1,35 +1,35 @@
 import os
 import sys
+import math
 import argparse
 from PIL import Image
 
 
-def resize_image(inputfile, outputdir, width, height, scale):
-    im = Image.open(inputfile)
-    old_width, old_height = im.size
-    old_filename, extension = os.path.split(inputfile)[-1].split('.')
-    if scale and width is None and height is None:
-        new_width, new_height = int(old_width * scale), int(old_height * scale)
-    elif width and height and scale is None:
-        new_width, new_height = width, height
-        if width / old_width != height / old_height:
-            print('New image has different scale')
-    elif width and scale is None:
-        scale = width / old_width
-        new_width, new_height = int(old_width * scale), int(old_height * scale)
-    elif height and scale is None:
-        scale = height / old_height
-        new_width, new_height = int(old_width * scale), int(old_height * scale)
+def resize_image(image, width, height):
+    return image.resize(size=(width, height))
+
+
+def validate_args(args):
+    if os.path.isfile(args.inputfile):
+        inputfile = args.inputfile
     else:
-        return None, None
-    new_name = '{old_filename}__{new_width}x{new_height}.{extension}'.format(
-        old_filename=old_filename,
-        new_width=new_width,
-        new_height=new_height,
-        extension=extension
-    )
-    im = im.resize(size=(new_width, new_height))
-    return im, new_name
+        sys.exit("Image file doesn't exist")
+    if args.outputdir and os.path.isdir(args.outputdir):
+        outputdir = args.outputdir
+    else:
+        outputdir = None
+    if any([args.width, args.height, args.scale]):
+        if args.scale and not any([args.width, args.height]):
+            scale = args.scale
+            width, height = args.width, args.height
+        elif (args.width or args.height) and not args.scale:
+            scale = args.scale
+            width, height = args.width, args.height
+        else:
+            sys.exit('Width and height parameters are not compatible with the scale')
+    else:
+        sys.exit('No parameters was provided for image resizing')
+    return inputfile, outputdir, scale,  width, height
 
 
 def create_argparser():
@@ -62,30 +62,46 @@ def create_argparser():
     return args
 
 
+def get_new_image_size(image, scale, width, height):
+    old_width, old_height = image.size
+    if scale and not any([width, height]):
+        new_width, new_height = int(old_width * scale), int(old_height * scale)
+    elif width and height and not scale:
+        new_width, new_height = width, height
+        if not math.isclose(width / old_width, height / old_height, rel_tol=1e-5):
+            print('New image has different scale')
+    elif width and not scale:
+        scale = width / old_width
+        new_width, new_height = int(old_width * scale), int(old_height * scale)
+    elif height and not scale:
+        scale = height / old_height
+        new_width, new_height = int(old_width * scale), int(old_height * scale)
+    return new_width, new_height
+
+
+def get_new_filename(old_filename, new_width, new_height):
+    filename, extension = os.path.splitext(old_filename)
+    new_filename = '{filename}__{new_width}x{new_height}{extension}'.format(
+        filename=filename,
+        new_width=new_width,
+        new_height=new_height,
+        extension=extension
+    )
+    return new_filename
+
+
 if __name__ == '__main__':
     argparser = create_argparser()
-    kwargs = vars(argparser)
-    if os.path.isfile(kwargs['inputfile']):
-        resized_image, resized_image_name = resize_image(**kwargs)
+    inputfile, outputdir, scale, width, height = validate_args(argparser)
+    image = Image.open(inputfile)
+    new_width, new_height = get_new_image_size(image, scale, width, height)
+    resized_image = resize_image(image, new_width, new_height)
+    old_filename = os.path.split(inputfile)[-1]
+    new_filename = get_new_filename(old_filename, new_width, new_height)
+    if outputdir:
+        outputfile = os.path.join(outputdir, new_filename)
     else:
-        print("File doesn't exist!")
-    if resized_image is None:
-        sys.exit('Incorrect or no arguments provided')
-    old_dir = os.path.split(kwargs['inputfile'])[0]
-    outputdir = kwargs['outputdir']
-    if kwargs['outputdir']:
-        resized_image.save(os.path.join(outputdir, resized_image_name))
-        print('Image is saved as {}'.format(
-            os.path.join(
-                outputdir,
-                resized_image_name
-            )
-        ))
-    else:
-        resized_image.save(os.path.join(old_dir, resized_image_name))
-        print('Image is saved as {}'.format(
-            os.path.join(
-                old_dir,
-                resized_image_name
-            )
-        ))
+        old_dir, _ = os.path.split(inputfile)
+        outputfile = os.path.join(old_dir, new_filename)
+    resized_image.save(outputfile)
+    print('Image is saved as {}'.format(outputfile))
